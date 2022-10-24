2ec1e114129302ddacc6e6b74b3b893538691919
Justin Yao Du <justinyaodu@gmail.com>
2022-10-24 14:48:08 -0700

    Add instructions to disable JS/TS extension

---
+++ solution.js
@@ -0,0 +1,9 @@
+/*
+ * Please disable the built-in extension "TypeScript and JavaScript Language Features":
+ * 1. Click the "Extensions" item on the left toolbar (it looks like three squares with a fourth popping out)
+ * 2. Search for "@builtin typescript"
+ * 3. Find "TypeScript and JavaScript Language Features" in the search results
+ * 4. Click the gear icon
+ * 5. Click "Disable"
+ * 6. Click the button that says "Reload Required"
+ */
