d018d765fab75b2404b1549a0f01effe45d8de7c
Justin Yao Du <justinyaodu@gmail.com>
2022-10-24 16:06:33 -0700

    Wording: item -> icon

--- solution.ts
+++ solution.ts
@@ -1,6 +1,6 @@
 /*
  * Please disable the built-in extension "TypeScript and JavaScript Language Features":
- * 1. Click the "Extensions" item on the left toolbar (it looks like three squares with a fourth popping out)
+ * 1. Click the "Extensions" icon on the left toolbar (it looks like three squares with a fourth popping out)
  * 2. Search for "@builtin typescript"
  * 3. Find "TypeScript and JavaScript Language Features" in the search results
  * 4. Click the gear icon
